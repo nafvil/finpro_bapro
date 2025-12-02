885328da0b51ed3d188d84c089cf72bd3e260f06
nafvil <naviltristan@gmail.com>
2025-12-02 13:25:40 +0700

    added base code

---
+++ code.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int houseCalc () {
+
+}
+
+int carDieselCalc () {
+
+}
+
+int carGasCalc () {
+
+}
+
+int carElectricCalc () {
+
+}
+
+int carHybridCalc () {
+
+}
+
+int motorGasCalc () {
+
+}
+
+int motorEletricCalc () {
+
+}
+
+int planeClac () {
+    
+}
+
+
+
+int main () {
+
+}
