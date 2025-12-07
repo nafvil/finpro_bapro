cfd041a5c8b48a7e94869754e34debbd4cd5c815
nafvil <naviltristan@gmail.com>
2025-12-07 16:38:35 +0700

    copied code from main

--- code.c
+++ code.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+// carbon emission calculator
 
+typedef struct {
+    float size;
+    char name[31];
+} area;
+// struct for area
+
+typedef struct {
+    float size;
+    float emmissions;
+} house;
+// struct for house size and emmisions
 
 
 int main () {
