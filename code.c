e18da727f88d30761d33f1fd1f1c31d54e6132a6
nafvil <naviltristan@gmail.com>
2025-12-07 16:26:52 +0700

    edited some files

--- code.c
+++ code.c
@@ -1,48 +1,6 @@
 #include <stdio.h>
 
 
-
-
-
-int main () {
-
-
-int houseCalc () {
-
-}
-
-int carDieselCalc () {
-
-}
-
-int carGasCalc () {
-
-}
-
-int carElectricCalc () {
-
-}
-
-int carHybridCalc () {
-
-}
-
-int motorGasCalc () {
-
-}
-
-int motorEletricCalc () {
-
-}
-
-int planeClac () {
-    
-}
-
-
-
-int main () {
-
     return 0;
 }
 
