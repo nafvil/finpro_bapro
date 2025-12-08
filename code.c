2aaf37b0f59b9c56c992999cca6933274d8f3f03
nafvil <naviltristan@gmail.com>
2025-12-08 19:35:39 +0700

    made some changes to the main code

---
+++ code.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+// carbon emission calculator
+
+typedef struct {
+    float areaSize;
+    char areaName[31];
+    int houseNum;
+    float emmissions;
+} Data;
+// struct for grouping the area data
+
+float CarbonEmmissionCalc(Data *data);
+float ClimateImpTree(float emmissionTons);
+float ClimateImpCoal(float emissionTons);
+
+int main () {
+    printf("=== Housing Area Carbon Emmission Calculator ===\n");
+    int count;
+    
+    // 1. INPUT SETUP
+    printf("Enter number of areas to analyze: ");
+    scanf("%d", &count);
+
+    Data data = (Data) malloc(count * sizeof(Data));
+    if (data == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+    // Input Loop
+    for (int i = 0; i < count; i++) {
+        printf("\nArea #%d\n", i + 1);
+        printf("Enter Area Name: ");
+        scanf("%s", data[i].areaName); // No & needed for strings (arrays)
+        printf("Enter Number of Houses: ");
+        scanf("%d", &data[i].numHouses);
+    }
+
+
+    return 0;
+}
