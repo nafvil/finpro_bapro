f966563db6572f0caf694c4018769ab5b1df94d1
nafvil <naviltristan@gmail.com>
2025-12-08 19:35:39 +0700

    deleted a lot of stuff but it'll work

--- code.c
+++ code.c
@@ -3,96 +3,20 @@
 // carbon emission calculator
 
 typedef struct {
-    float size;
-    char name[31];
-} area;
-// struct for area
-
-typedef struct {
-    float amount;
-    float size;
+    float areaSize;
+    char areaName[31];
+    int houseNum;
     float emmissions;
-} house;
-// struct for house 
+} Data;
+// struct for grouping the area data
 
-float RegMap (float a; float b){
-    return a * b;
-
-float RegArea (float AvH){
-        AvH - //in Meters
-    return (RegMap * 1000) / AvH 
-    
-}
-
-float RegCalc (float C){
-    C = 55;
-            //in tons
-    return RgArea * Carbons;
-}
-
-float ClimateImpTree(float a; float b; float c; float d; float RegCalc; float EqA; ){
-    a =  0.11; //trees sampled in an urban settings
-    b =  23.2; //coniferous tree/year
-    c =  0.89; //Percent of deciduous trees in sampled Urban settings
-    d =  38;
-        
-    EqA = (a * b) + (c * d);
-        
-    
-    return e / (EqA / 2000);
-
-
-    /* (0.11 [percent of coniferous trees in sampled urban settings] × 23.2 lbs C/coniferous tree/year) + (0.89 [percent of deciduous trees in sampled urban settings] × 38.0 lbs C/deciduous tree/year) = 36.4 lbs C/tree/year
-Average Houses Carbon Footprint: */
-}
-
-
-float ClimateImpCoal(float a; float b; float c; float d; float e;){
-    a = ;
-    b = ;
-    c = ;
-    d = ;
- 
-    //746,630,725.6 metric tons of CO2 × 1/197 power plants = 3,790,003.72 metric tons CO2/power plant
-
-}
-
+float CarbonEmmissionCalc(Data *data);
+float ClimateImpTree(float emmissionTons);
+float ClimateImpCoal(float emissionTons);
 
 int main () {
-
- return 0;
-}
+    printf("=== Housing Area Carbon Emmission Calculator ===\n");
 
 
-/* PSEUDOCODE:
-insert function here
-
-int main(){
-    int i;
-    int a = **a;
-
-    calloc malloc whatever
-    print("Input Jumlah Area");
-    scanf("%f", &b);
-    call function RegCalc;
-    printf("Input jumlah Rumah");
-    scanf("%f", &b);
-    call function rumah etc;
-    call function RegArea;
-    call function RegCalc;
-    
-    call function ClimateImpTree;
-    printf("Equivalent Trees Planted for 10 years");
-    call function ClimateImpCoal;
-    printf("Equivalent Coal powered plant fired up");
-    call function ClimateImpNat;
-    printf("Equivalent Natural powered plant fired up");
-    
-    
     return 0;
-    
-}
-
-*/
- return 0;
 }
