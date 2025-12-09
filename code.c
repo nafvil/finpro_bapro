739e02a9bc478488655702ecf20e7573f9d8f00d
potuerza <majiid.vadas22@gmail.com>
2025-12-09 10:23:42 +0700

    Improving Structure and giving Instructions

--- code.c
+++ code.c
@@ -17,6 +17,11 @@ float ClimateImpCoal(float emissionTons);
 
 int main () {
     printf("=== Housing Area Carbon Emission Calculator ===\n");
+    printf("This Program Converts the amount of CO2 emission in one area to give an image about how much the Area contributed to Climate Changes.");
+    printf("\nYou will be asked to enter the area name, size, number of houses, and average emissions of all the houses in tons.");
+    printf("\nThe program will then calculate the total emissions, number of trees needed to offset the emissions, and equivalent coal burned.\n");
+    printf("=================================================\n");
+
     int count;
     printf("Enter number of areas to analyze: ");
     scanf("%d", &count);
@@ -40,10 +45,21 @@ int main () {
     }
 
     printf("\n=== Climate Impact ===\n");
-    printf("");
-    
+
+    for (int i = 0; i < count; i++) {
+        float totalEmissions = CarbonEmissionCalc(&data[i]);
+        float TreeImpact = ClimateImpTree(totalEmissions);
+        float CoalImpact = ClimateImpCoal(totalEmissions);
+
+
+        printf("\nArea: %s\n", data[i].areaName);
+        printf("Total Emissions: %.2f tons\n", totalEmissions);
+        printf("Trees needed to offset emissions: %.2f\n", TreeImpact); 
+        printf("Equivalent coal burned: %.2f tons\n", CoalImpact);
+
+    }
+
     return 0;
-    
 }
 
 float CarbonEmissionCalc(Data *data) {
