893af53fa1ab10f9be5484a8ab9fd04435fe0a6f
nafvil <naviltristan@gmail.com>
2025-12-09 09:55:31 +0700

    copied contents from main branch

--- code.c
+++ code.c
@@ -1,31 +1,51 @@
-/* PSEUDOCODE:
-insert function here
+#include <stdio.h>
 
-int main(){
-    int i;
-    int a = **a;
+// carbon emission calculator
 
-    calloc malloc whatever
-    print("Input Jumlah Area");
-    scanf("%f", &b);
-    call function RegCalc;
-    printf("Input jumlah Rumah");
-    scanf("%f", &b);
-    call function rumah etc;
-    call function RegArea;
-    call function RegCalc;
-    
-    call function ClimateImpTree;
-    printf("Equivalent Trees Planted for 10 years");
-    call function ClimateImpCoal;
-    printf("Equivalent Coal powered plant fired up");
-    call function ClimateImpNat;
-    printf("Equivalent Natural powered plant fired up");
-    
+typedef struct {
+    float areaSize;
+    char areaName[31];
+    int houseNum;
+    float houseEmissions;
+    float totalEmissions;
+} Data;
+// struct for grouping the area data
+
+float CarbonEmissionCalc(Data *data);
+float ClimateImpTree(float emissionTons);
+float ClimateImpCoal(float emissionTons);
+
+int main () {
+    printf("=== Housing Area Carbon Emission Calculator ===\n");
+    int count;
+    printf("Enter number of areas to analyze: ");
+    scanf("%d", &count);
+
+    Data *data = (Data*) malloc(count * sizeof(Data));
+    if (data == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < count; i++) {
+        printf("\nArea #%d\n", i + 1);
+        printf("Enter area name: ");
+        scanf("%s", data[i].areaName);
+        printf("Enter area size: ");
+        scanf("%d", &data[i].areaSize);
+        printf("Enter Number of houses: ");
+        scanf("%d", &data[i].houseNum);
+        printf("Enter the average emissions of all the houses: ");
+        scanf("%d", &data[i].houseEmissions);
+    }
+
+    printf("\n=== Climate Impact ===\n");
+    printf("");
     
     return 0;
     
 }
 
-*/
-
+float CarbonEmissionCalc(Data *data) {
+    return data
+}
